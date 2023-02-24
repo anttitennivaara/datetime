5d4d74d2b30c323d8bf2319aee4b3cbb8f1cdd17
Antti Tennivaara <antti.tennivaara@gmail.com>
2023-02-24 09:17:52 +0000

    Remove tests from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
 pub mod datetime_struct;
 pub mod time_struct;
 pub mod functions;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn time_struct_test() {
-        use crate::time_struct::build_time;
-        let mut test_time = build_time(1_234_456, "nanoseconds");
-        assert_eq!(test_time.to_unit("microseconds"), 1234);
-        test_time.add_time(1, "seconds");
-        assert_eq!(test_time.to_unit("microseconds"), 2234);
-        for _ in 0..1_000_000_000 {
-            println!("{}", test_time.to_string());
-            test_time.add_time(10, "microseconds");
-        }
-    }
-}
