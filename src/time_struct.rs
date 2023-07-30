10900e28de2e267ee49ce9382262cdc9a7fffa4f
Antti Tennivaara <antti.tennivaara@gmail.com>
2023-07-30 21:16:39 +0000

    Implement PartialEq and PartialOrd for Time

--- src/time_struct.rs
+++ src/time_struct.rs
@@ -34,6 +34,18 @@ impl SubAssign for Time {
     }
 }
 
+impl PartialEq for Time {
+    fn eq(&self, other: &Self) -> bool {
+        self.nanoseconds == other.nanoseconds
+    }
+}
+
+impl PartialOrd for Time {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.nanoseconds.partial_cmp(&other.nanoseconds)
+    }
+}
+
 impl fmt::Display for Time {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let hours = self.to_unit("hours");
@@ -63,30 +75,8 @@ impl Time {
         }
     }
 
-    // pub fn to_string(&self) -> String {
-    //     let hours = self.to_unit("hours");
-    //     let minutes = self.to_unit("minutes") - hours_to_minutes(hours);
-    //     let seconds = self.to_unit("seconds") - minutes_to_seconds(self.to_unit("minutes"));
-    //     let mut time_string = String::from("");
-    //     time_string.push_str(leading_zero_string(hours, 2).as_str());
-    //     time_string.push_str(":");
-    //     time_string.push_str(leading_zero_string(minutes, 2).as_str());
-    //     time_string.push_str(":");
-    //     time_string.push_str(leading_zero_string(seconds, 2).as_str());
-    //     time_string.push_str(".");
-    //     time_string.push_str(leading_zero_string(self.nanoseconds, 6).as_str());
-    //     time_string
-    // }
-
-    pub fn add_time(&mut self, time: isize, unit: &str) {
-        self.nanoseconds += match unit {
-            "nanoseconds" => time,
-            "microseconds" => microseconds_to_nanoseconds(time),
-            "seconds" => seconds_to_nanoseconds(time),
-            "minutes" => minutes_to_nanoseconds(time),
-            "hours" => hours_to_nanoseconds(time),
-            _ => panic!("Invalid unit"),
-        }
+    pub fn add_time(mut self, time: isize, unit: &str) {
+        self += build_time(time, unit);
     }
 }
 
